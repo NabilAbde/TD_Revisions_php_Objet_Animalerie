18fd03fc25e4aff218e8f454b2b3876bba51b8b5
[nabil] <[consonab@gmail.com]>
2021-06-02 11:12:05 +0100

    Ajout categories

--- model/category.php
+++ model/category.php
@@ -1,9 +1,14 @@
-<?php 
-    $categorie = [
-        'alimentation',
-        'niches et couchage',
-        'transport',
-        'hygiène et soin',
-        'Gamelles et accessoires',
-        'Jouets'
-    ];
+<?php
+$categorie = [
+    'alimentation',
+    'niches et couchage',
+    'transport',
+    'hygiène et soin',
+    'Gamelles et accessoires',
+    'Jouets',
+    'Cages et accessoires',
+    'Entretiens de l’eau',
+    'Décorations de l’aquarium',
+    'Bassins',
+    'Aquariums et meubles',
+];
