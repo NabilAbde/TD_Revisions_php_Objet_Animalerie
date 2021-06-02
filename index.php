f3fe7a41d99bd425ed0c74f86f5b3308bac499e5
Nathan <nathan.flacher.pro@gmail.com>
2021-06-02 11:49:11 +0200

    modif index

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
-<?php
-    require('model/animal.php');
-    require('model/brand.php');
-    require('model/category.php');
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
     <title>Revision</title>
 </head>
 <body>
-    <h1> Animalerie </h1>
+    <header>
+        <h1>L'Animalerie</h1>
+    </header>
+    
+    <?php include("view/vueAnimal.php"); ?>
+    <?php include("view/vueBrand.php"); ?>
+    <?php include("view/vueCategory.php"); ?>
 </body>
 </html>
