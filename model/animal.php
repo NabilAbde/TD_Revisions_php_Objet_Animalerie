efd9a628fa9bdde1d6cf50dbc878d0b7c82fc451
Nathan <nathan.flacher.pro@gmail.com>
2021-06-02 11:09:54 +0200

    modif animal.php

--- model/animal.php
+++ model/animal.php
@@ -1,3 +1,16 @@
 <?php
-$tabAnimaux = array("Chien","Chat","Aquariophilie","Oiseaux","Rongeurs","Reptiles");
+$tabAnimaux = array(
+    "Chien",
+    "Chat",
+    "Aquariophilie",
+    "Oiseaux",
+    "Rongeurs",
+    "Reptiles",
+    "Naoty",
+    "Sera",
+    "JBL",
+    "Tetra",
+    "Tyrol",
+    "Supreme"
+);
 var_dump($tabAnimaux);
