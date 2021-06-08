08b339050677b1750bdcdb1c9eb63f406c66bc27
Nathan <nathan.flacher.pro@gmail.com>
2021-06-08 10:20:13 +0200

    creation classe category

---
+++ model/classeCategory.php
@@ -0,0 +1,13 @@
+<?php
+
+    class category{
+        private $_name;
+        private $_picture = NULL;
+        private $_description = 'non defini';
+        
+        public function __construct(){
+            $this->_name = '' ;
+            $this->_picture = '' ;
+            $this->_description = '' ;
+        }
+    }
