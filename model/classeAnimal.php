8b190fd695d37af21a64aa0f714a07374888b10e
[nabil] <[consonab@gmail.com]>
2021-06-08 10:23:28 +0100

    creation classe animale

---
+++ model/classeAnimal.php
@@ -0,0 +1,16 @@
+<?php
+class Animal{
+
+    private $quatrePattes;
+    private $deuxPattes;
+    private $sansPatte;
+    private $avecBec;
+    private $avecNageoire;
+    
+    public function __construct()
+    {
+        $this->quattrePattes = "Chien";
+        $this->deuxPattes ="false";
+    }
+
+}
